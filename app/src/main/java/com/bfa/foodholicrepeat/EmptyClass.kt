e03aee5ff7924b5c7e37f2075de3ac312e0cd17b
Blair Felix Anachoni <felixanachoni@gmail.com>
2021-11-05 23:05:16 +0300

    An extra class

---
+++ app/src/main/java/com/bfa/foodholicrepeat/EmptyClass.kt
@@ -0,0 +1,7 @@
+package com.bfa.foodholicrepeat
+
+class EmptyClass {
+    //this class is an Array and just to practise pushing to github
+    var name2: String? = null
+    var me: String? = null
+}
